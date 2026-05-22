ae7c462a460b8eb70002c9b5be7a52a5b881aa6c
Stephen Severino <muffledcry@users.noreply.github.com>
2026-05-22 16:11:58 -0400

    Module added to tracked files

---
+++ scoreform/config.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+IMG_WIDTH = 1275
+IMG_HEIGHT = 1650
+CORNER_SIZE = 50
+
+# Corner coordinates (Top-Left, Top-Right, Bottom-Left, Bottom-Right)
+CORNERS = [
+    (100, 100),
+    (IMG_WIDTH - 100 - CORNER_SIZE, 100),
+    (100, IMG_HEIGHT - 100 - CORNER_SIZE),
+    (IMG_WIDTH - 100 - CORNER_SIZE, IMG_HEIGHT - 100 - CORNER_SIZE),
+]
+
+# Centers of the corners for perspective transform (TL, TR, BL, BR)
+DST_PTS = np.array(
+    [
+        [100 + CORNER_SIZE // 2, 100 + CORNER_SIZE // 2],
+        [IMG_WIDTH - 100 - CORNER_SIZE // 2, 100 + CORNER_SIZE // 2],
+        [100 + CORNER_SIZE // 2, IMG_HEIGHT - 100 - CORNER_SIZE // 2],
+        [IMG_WIDTH - 100 - CORNER_SIZE // 2, IMG_HEIGHT - 100 - CORNER_SIZE // 2],
+    ],
+    dtype="float32",
+)
+
+# Question layout
+Q_START_Y = 400
+Q_STEP_Y = 80
+BOX_SIZE = 30
+BOX_START_X = 300
+BOX_STEP_X = 120
+
+PDF_WIDTH = 612
+PDF_HEIGHT = 792
+PDF_SCALE = PDF_WIDTH / IMG_WIDTH
